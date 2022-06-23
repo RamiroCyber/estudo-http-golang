151a79a09b366089173984e9dbe9a1400891f041
RamiroCyber <ramiro.dr@gmail.com>
2022-06-22 23:29:03 -0300

    comitando

---
+++ http/dynamic/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func RightTime(w http.ResponseWriter, r *http.Request) {
+	timeNow := time.Now().Format("02/01/2006 03:04:05")
+	fmt.Fprintf(w, "Hora certa: %s", timeNow)
+
+}
+
+func main() {
+	http.HandleFunc("/", RightTime)
+	log.Println("Executando....")
+	log.Fatal(http.ListenAndServe(":4000", nil))
+}
