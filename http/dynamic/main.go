b895394be063186620d1ee35ab41d2aea18bbbd6
RamiroCyber <ramiro.dr@gmail.com>
2022-06-26 22:24:14 -0300

    comitando

--- http/dynamic/main.go
+++ http/dynamic/main.go
@@ -9,7 +9,7 @@ import (
 
 func RightTime(w http.ResponseWriter, r *http.Request) {
 	timeNow := time.Now().Format("02/01/2006 03:04:05")
-	fmt.Fprintf(w, "Hora certa: %s", timeNow)
+	fmt.Fprintf(w, "<h1>Hora certa: %s</h1>", timeNow)
 
 }
 
---
+++ http/serverdb/cliente.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	json2 "encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type User struct {
+	ID   int64  `json:"id"`
+	Nome string `json:"nome"`
+}
+
+func userId(w http.ResponseWriter, r *http.Request, id int) {
+	db, err := sql.Open("mysql", "rrot:123456/httpgo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var user User
+
+	db.QueryRow("select id,nome from users where id = ? ", id).Scan(&user.ID, &user.Nome)
+
+	json, _ := json2.Marshal(user)
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(json))
+}
+
+func allUsers(w http.ResponseWriter, r *http.Request, id int) {
+
+}
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	sid := strings.TrimPrefix(r.URL.Path, "/users")
+	id, _ := strconv.Atoi(sid)
+
+	switch {
+	case r.Method == "GET" && id > 0:
+		userId(w, r, id)
+	case r.Method == "GET":
+		allUsers(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+}
